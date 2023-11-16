a84b11b3084dac273348ddb553b1001dcd296bcd
Michael Mote <michael.mote1993@gmail.com>
2023-11-16 15:07:30 -0600

    fixed listen to local db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 const { Pool } = require('pg')
 
 dotenv.config()
+const app = express()
 
+//env variables
 const dbString = process.env.DATABASE_URL
 const PORT = process.env.PORT
 
@@ -11,8 +14,16 @@ const pool = new Pool({
     connectionString: dbString
 })
 
-const app = express()
+//middleware
+app.use(cors())
+app.use(express.json())
 
-app.listen(PORT, () => {
-    console.log(`You are now tuned into port ${PORT}.`)
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+const port = 1337
+app.listen(port, () => {
+    console.log(`You are now tuned into port ${port}.`)
 })
